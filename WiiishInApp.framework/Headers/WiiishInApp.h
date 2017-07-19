c95d7f70e047cdfb9e4dfa275ac9dd144e550193
Francois Reboursier <francois.reboursier@ezeeworld.com>
2017-07-19 15:52:26 +0200

    Publish SDK 1.0.0

---
+++ WiiishInApp.framework/Headers/WiiishInApp.h
@@ -0,0 +1,20 @@
+//
+//  WiiishInApp.h
+//  WiiishInApp
+//
+//  Created by Stéphanie Spenlé on 21/06/2017.
+//  Copyright © 2017 Areas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WiiishInApp.
+FOUNDATION_EXPORT double WiiishInAppVersionNumber;
+
+//! Project version string for WiiishInApp.
+FOUNDATION_EXPORT const unsigned char WiiishInAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WiiishInApp/PublicHeader.h>
+
+
+#import <WiiishInApp/WiiishManager.h>
---
+++ WiiishInApp.framework/Headers/WiiishManager.h
@@ -0,0 +1,34 @@
+//
+//  WiiishManager.h
+//  WiiishInApp
+//
+//  Created by Stéphanie Spenlé on 21/06/2017.
+//  Copyright © 2017 Areas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol WiiishInAppDelegate;
+
+@interface WiiishManager : NSObject
+
+@property (nonatomic, weak) id<WiiishInAppDelegate> delegate;
+
++(WiiishManager*)sharedManager;
+
++(void)initialize;
++(void)displayWiiish;
++(void)closeWiiish;
++(void)updateTheme:(NSString*)newTheme;
+@end
+
+
+
+
+@protocol WiiishInAppDelegate <NSObject>
+
+@optional
+-(void)wiiishViewDidOpen;
+-(void)wiiishViewDidClose;
+
+@end
