00510f3d2bede0bfc4b71b736202898699ec11e3
freboursier-ez <francois.reboursier@ezeeworld.com>
2017-10-02 17:03:02 +0200

    Publish SDK 1.2.0

--- WiiishInApp.framework/Headers/WiiishManager.h
+++ WiiishInApp.framework/Headers/WiiishManager.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @protocol WiiishInAppDelegate;
 
@@ -17,8 +18,12 @@
 + (WiiishManager*)sharedManager;
 + (void)prime;
 + (void)displayWiiish;
++ (void)displayWiiishWithOrderId:(NSString*)orderId;
 + (void)closeWiiish;
 + (void)updateTheme:(NSString*)newTheme;
++ (BOOL)handleUrl:(NSURL*)url;
++ (void)setTitlebarColor:(UIColor*)newColor;
+    
 @end
 
 
